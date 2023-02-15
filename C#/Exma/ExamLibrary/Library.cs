69c310e490a40a6fd9257e17339ceddc586079be
Karylus <juanpemoreno@correo.ugr.es>
2023-02-15 17:24:00 +0100

    Added Exam Folder

---
+++ C#/Exma/ExamLibrary/Library.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamLibrary
+{
+    public class Entertainment
+    {
+        public Entertainment(string name, string director, string genre, string date)
+        {
+            _name = name;
+            _director = director;
+            _genre = genre;
+            _date = date;
+        }
+
+        protected string _name;
+        protected string _director;
+        protected string _genre;
+        protected string _date;
+
+        //Setters and getters
+        public string Name 
+        { 
+            get { return _name; } 
+            set { _name = value; }
+        }
+        public string Director
+        {
+            get { return _director; }
+            set { _director = value; }
+        }
+        public string Genre
+        {
+            get { return _genre; }
+            set { _genre = value; }
+        }
+        public string Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        //Public methods
+        public virtual void Show()
+        {
+            Console.WriteLine("---------------------------------");
+
+            Console.WriteLine(" The name is: {0}\n\t The director is: {1}\n\t The genre is: {2}\n\t The date of production is: {3}", this.Name, this.Director, this.Genre, this.Date);
+
+            Console.WriteLine("---------------------------------");
+        }
+    }
+
+    public class Performance : Entertainment
+    {
+        public Performance (string name, string director, string genre, string date, string type, List<string> characters, double price) : base(name, director, genre, date)
+        {
+            _type = type;
+            _characters = characters;
+            _price = price;
+        }
+
+        public Performance(string name, string director, string genre, string date, string type, List<string> characters, string place, int time) : base(name, director, genre, date)
+        {
+            _type = type;
+            _characters = characters;
+            _place = place;
+            _time = time;
+        }
+
+        protected string _type;
+        protected List<string> _characters;
+        protected double _price;
+        protected string _place;
+        protected int _time;
+
+        //Setters and getters
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+        public List<string> Characters
+        {
+            get { return _characters; }
+            set { _characters = value; }
+        }
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public string Place
+        {
+            get { return _place; }
+            set { _place = value; }
+        }
+        public int Time
+        {
+            get { return _time; }
+            set { _time = value; }
+        }
+
+        //Public methods
+        public override void Show()
+        {
+            if (this.Type == "Drama")
+            {
+                Console.WriteLine("---------------------------------");
+
+                Console.WriteLine(" The name is: {0}\n\t The director is: {1}\n\t The genre is: {2}\n\t " +
+                    "The date of production is: {3}\n\t The theatre is: {4}\n\t " +
+                    "It lasts (minutes): {5}", this.Name, this.Director, this.Genre, this.Date, this.Place, this.Time);
+
+                Console.WriteLine("---------------------------------");
+            }
+            else if (this.Type == "Film")
+            {
+                Console.WriteLine("---------------------------------");
+
+                Console.WriteLine(" The name is: {0}\n\t The director is: {1}\n\t The genre is: {2}\n\t " +
+                    "The date of production is: {3}\n\t The tickets price is: {4}\n\t " +
+                    "", this.Name, this.Director, this.Genre, this.Date, this.Price);
+
+                Console.WriteLine("---------------------------------");
+            }
+        }
+    }
+
+}
---
+++ C#/Exma/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExamLibrary;
+
+namespace Exam
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Entertainment show1 = new Entertainment("Inglourious Basterds", "Tarantino", "Action", "April 2009");
+            Entertainment show2 = new Entertainment("The Shape of Water", "Del Toro", "Fantasy", "October 2017");
+            Entertainment show3 = new Entertainment("The Fog", "Carpenter", "Terror", "April 1980");
+
+            show1.Show();
+            show3.Show();
+
+            string char1 = "Geralt De Rivia";
+            string char2 = "Joel";
+            string char3 = "Kratos";
+
+            List<string> characters = new List<string>
+            {
+                char1,
+                char2,
+                char3
+            };
+
+            Performance film1 = new Performance("Mulholland Drive", "Lynch", "Mistery", "January 2001", "Film", characters, 14.5);
+            Performance film2 = new Performance("The Batman", "Reeves", "Action", "June 2022", "Film", characters, 6.75);
+
+            Performance play = new Performance("The House of Bernarda Alba", "Lorca", "Romantic", "July 1945", "Drama", characters, "Royal Theatre", 185);
+
+            film1.Show();
+            play.Show();
+
+            List<Entertainment> shows = new List<Entertainment> { show1, show2, show3 };
+            List<Performance> performances = new List<Performance> { film1, film2, play };
+
+            //*****************************************************************
+            //Sorting and saving shows
+            var result = from show in shows
+                          orderby show.Name ascending
+                          select show;
+
+            List<string> lines = new List<string> { };
+
+            lines.Add("----------------------------------------");
+            lines.Add("Shows sorted by Name:\n");
+
+            foreach (Entertainment show in result)
+            {
+                string all = "\t" + show.Name + " | " + show.Director;
+                lines.Add(all);
+            }
+
+            lines.Add("----------------------------------------");
+
+            using (StreamWriter outputFile = new StreamWriter("SortedShows.txt"))
+            {
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
+            }
+            //*****************************************************************
+
+            //*****************************************************************
+            //Saving and sorting performances
+            var result2 = from performance in performances
+                         orderby performance.Name ascending
+                         select performance;
+
+            List<string> lines2 = new List<string> { };
+
+            lines2.Add("----------------------------------------");
+            lines2.Add("Performances sorted by Name:\n");
+
+            foreach (Performance performance in result2)
+            {
+                string all = "\t" + performance.Name + " | " + performance.Director + " | " + performance.Type;
+                lines2.Add(all);
+            }
+
+            lines2.Add("----------------------------------------");
+
+            using (StreamWriter outputFile = new StreamWriter("SortedPerformances.txt"))
+            {
+                foreach (string line in lines2)
+                    outputFile.WriteLine(line);
+            }
+            //*****************************************************************
+        }
+    }
+}
